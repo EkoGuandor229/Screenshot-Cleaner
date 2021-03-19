1b14cebdf26a6fb2110b355ecf67585c2284db34
Eko229 <ekoguandor229@gmail.com>
2021-03-19 16:04:45 +0100

    Finish Logic

--- ScreenshotCleaner.py
+++ ScreenshotCleaner.py
@@ -1,37 +1,30 @@
 from PIL import Image
-import numpy as np
-
-
+import glob
 
 
 def getpositionoffirstblackpixelinrange(image, start, stop, step):
     for x in range(start, stop, step):
         if image.getpixel((1, x)) >= (5, 5, 5):
             return x
-
-
-
-image = Image.open("Unbearbeitete Bilder/Screenshot_20200826-113200.jpg")
+        elif image.getpixel((image.size[0]-1, x)) >= (5, 5, 5):
+            return x
 
 
 def crop_image(image):
-    global cropped_image
     upper_crop_level = getpositionoffirstblackpixelinrange(image, 0, image.size[1], 1)
     lower_crop_level = getpositionoffirstblackpixelinrange(image, image.size[1] - 1, 0, -1)
-    box = (0, upper_crop_level, image.size[0], lower_crop_level)
+    box = (
+        0,
+        upper_crop_level,
+        image.size[0]-1,
+        lower_crop_level
+    )
     return image.crop(box)
 
 
-cropped = crop_image(image)
-cropped.show()
-
-
-
-
-
-# 1. Load all images
-# 2. For each image From Top to bottom:
-#   Check the leftmost rightmost and middle pixel
-#   If l == r == m != Black -> get pixel position
-#   Crop pixel at that position
-# 3. From Bottom to Top -> same procedure
+for file in glob.glob("Unbearbeitete Bilder/*.jpg"):
+    image_name = file[21:]
+    image = Image.open(file)
+    cropped = crop_image(image)
+    cropped.save("Korrigierte Bilder/" + image_name)
+    print(image_name)
